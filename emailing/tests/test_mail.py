443e4aabf0ed6ff3d673fa7be82351f1f3e25eb5
Chris Bader <chris.bader@schnapptack.de>
2017-04-21 17:08:00 +0200

    added templates

--- emailing/tests/test_mail.py
+++ emailing/tests/test_mail.py
@@ -5,22 +5,23 @@ from emailing import emails
 from emailing.emails import HtmlEmail
 from django_productline.testingutils import NoMigrationsTestCase
 from premailer import transform
+from emailing.emails import send
 
 
 class SignupMailTest(NoMigrationsTestCase):
     """
     This is a helper test class which allows us to programmatically send emails for proper viewing
     """
-
     @override_settings(EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend')
-    def test_send_email(self):
+    def test_send_email(self, **kwargs):
         from django.conf import settings
         context = dict(
             brand_bg='#fff',
             body_bg='#eee',
             brand_color='#575757',
             link_color='#2244DD',
-            footer_content='Welcome, this is a mail'
+            footer_content='Welcome, this is a mail',
+            username=kwargs.pop('username')
         )
 
         msg = HtmlEmail(
@@ -31,3 +32,4 @@ class SignupMailTest(NoMigrationsTestCase):
         )
         msg.content_subtype = 'html'
         msg.send(fail_silently=False)
+
